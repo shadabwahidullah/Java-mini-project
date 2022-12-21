330298234cac9eb87d80a8f40e2e528219d061b0
shadabwahidullah <shadabwahidullah@gmail.com>
2022-12-21 10:44:41 +0430

    feat: add printMenu

--- Project.java
+++ Project.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 class Project {
   public static void main(String[] args) {
-    System.out.println("hello world");
+    Scanner scn = new Scanner(System.in);
+    int menuOption = 0;
+    while(menuOption != 4){
+      printMenu();
+      menuOption = scn.nextInt();
+    }
+  }
+
+  public static void printMenu () {
+    System.out.println("MENU");
+    System.out.println("1. Add a book");
+    System.out.println("2. Buy a book");
+    System.out.println("3. Search for a book using title");
+    System.out.println("4. Exit\n");
   }
 }
