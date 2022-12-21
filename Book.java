7e23f05c4c65c79bcb2f50eed7199c613c3ebf21
shadabwahidullah <shadabwahidullah@gmail.com>
2022-12-21 11:03:08 +0430

    feat: add book class

--- Book.java
+++ Book.java
@@ -1,3 +1,16 @@
 public class Book {
+  String title;
+  String author;
+  String publisher;
+  int position;
+  double boughtPrice;
+
+  public Book (String title, String author, String publisher, int position, double boughtPrice) {
+    this.title = title;
+    this.author = author;
+    this.publisher = publisher;
+    this.position = position;
+    this.boughtPrice = boughtPrice;
+  }
 
 }
